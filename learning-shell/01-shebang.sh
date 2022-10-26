68536ec1677c760d3635feb448619bd495fc06b2
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2022-10-25 22:19:49 -0400

    example 2 added

---
+++ learning-shell/01-shebang.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# comment  - it will just ignore, this is used to read the code in a better way 
+# #! is called as she-bang
+# Completely optional
+# adding that will have an understanding of what script it is
+
+ls
+
+# shell script is nothing but adding all your manual commands in a order
--- learning-shell/example.sh
+++ learning-shell/example.sh
@@ -1,4 +1,13 @@
-mkdir abcd 
+#!/bin/bash 
+
+ # This command will create a directory 
+mkdir abcd
+
+#chnage directory to abcd 
 cd abcd 
+
+#create a file called movie.txt 
 touch movie.txt 
+
+#add contents to the file movie.txt 
 echo "magicbricks" >> movie.txt 
