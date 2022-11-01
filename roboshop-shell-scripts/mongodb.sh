2abcbb6acd2d3ceaf1065740d8af94c0c2b0c6b7
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2022-10-31 21:35:09 -0400

    updated the script

--- roboshop-shell-scripts/mongodb.sh
+++ roboshop-shell-scripts/mongodb.sh
@@ -2,29 +2,29 @@
 echo "Mongo script to install"
 COMPONENT=mongodb
 LOG=/tmp/${COMPONENT}
-source common.sh
+source ./common.sh
 
-PRINT "Configure YUM Repos"
+echo "Configure YUM Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
 StatusCheck $?
 
-PRINT "Install MongoDB"
+echo "Install MongoDB"
 yum install -y -nogpgcheck mongodb-org &>>${LOG}
 StatusCheck $?
 
-PRINT "Configure MongoDB Service"
+echo "Configure MongoDB Service"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 StatusCheck $?
 
-PRINT "Start MonogDB"
+echo "Start MonogDB"
 systemctl enable mongod &>>${LOG}  && systemctl restart mongod &>>${LOG}
 StatusCheck $?
 
 
-PRINT "Download Schema"
+echo "Download Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG}
 StatusCheck $?
 
-PRINT "Load Schema"
+echo "Load Schema"
 cd /tmp && unzip -o mongodb.zip &>>${LOG}  && cd mongodb-main && mongo < catalogue.js &>>${LOG}  && mongo < users.js &>>${LOG}
 StatusCheck $?
